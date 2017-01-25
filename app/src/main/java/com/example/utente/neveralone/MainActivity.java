7ba8105e5b03aefa94bb9102bbaa05ec75ebd47c
Francesco Di Leo <Francesco Di Leo>
2017-01-25 11:29:41 +0100

    creazione prima ac7tivity

---
+++ app/src/main/java/com/example/utente/neveralone/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.utente.neveralone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Utente on 25/01/2017.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
